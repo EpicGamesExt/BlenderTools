3ed18805613035d8b5ac4c3c04fde3bd734a11bb
Doug Richardson <git@rekt.email>
2020-12-13 17:53:27 -0800

    Move asset cleanup to setUp and tearDown so that each test is run in a known state.
    Fix test logic when importing with materials turned off.

--- test/unit_tests/send2ue_validate_materials_and_textures.py
+++ test/unit_tests/send2ue_validate_materials_and_textures.py
@@ -20,9 +20,15 @@ class Send2UeMaterialToUnreal(unittest.TestCase):
         # enable the required addons
         bpy.ops.preferences.addon_enable(module='send2ue')
 
+        # Make sure the import area is clean
+        unreal_utilities.delete_directory('/Game/untitled_category/untitled_asset')
+
     def tearDown(self):
         # restore blend file to the default test file
         bpy.ops.wm.open_mainfile(filepath=os.path.join(os.environ['BLENDS'], 'default_startup.blend'))
+
+        # Cleanup the import area after we are done.
+        unreal_utilities.delete_directory('/Game/untitled_category/untitled_asset')
 
     def test_send_material_to_unreal_true(self):
         """
@@ -44,9 +50,6 @@ class Send2UeMaterialToUnreal(unittest.TestCase):
         self.assertTrue(unreal_utilities.asset_exists('/Game/untitled_category/untitled_asset/Cube'))
         self.assertTrue(unreal_utilities.asset_exists('/Game/untitled_category/untitled_asset/Material'))
 
-        # delete all the assets created by the import
-        unreal_utilities.delete_directory('/Game/untitled_category/untitled_asset')
-
         # move the cube out of the mesh collection
         bpy.context.scene.collection.objects.link(cube)
         bpy.data.collections['Mesh'].objects.unlink(cube)
@@ -67,14 +70,10 @@ class Send2UeMaterialToUnreal(unittest.TestCase):
         # run the send to unreal operation
         bpy.ops.wm.send2ue()
 
-        # check if the cube and material exists in the unreal project
+        # check if the cube exists in the unreal project
         self.assertTrue(unreal_utilities.asset_exists('/Game/untitled_category/untitled_asset/Cube'))
-
-        if properties == False:
-            self.assertTrue(unreal_utilities.asset_exists('/Game/untitled_category/untitled_asset/Material')) == False
-
-        # delete all the assets created by the import
-        unreal_utilities.delete_directory('/Game/untitled_category/untitled_asset')
+        # make sure the material does not exist in the unreal project
+        self.assertFalse(unreal_utilities.asset_exists('/Game/untitled_category/untitled_asset/Material'))
 
         # move the cube out of the mesh collection
         bpy.context.scene.collection.objects.link(cube)
