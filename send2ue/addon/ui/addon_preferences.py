c3e92bf242a7464d3524fd04c8474f8b89562454
Michael Klement <klement@little-things.de>
2021-04-06 20:29:31 +0200

    Refactored settings into functions to improve readability

--- send2ue/addon/ui/addon_preferences.py
+++ send2ue/addon/ui/addon_preferences.py
@@ -26,29 +26,48 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
         row.prop(properties, 'options_type', expand=True)
 
         if properties.options_type == 'general':            
+            self.draw_general_tab(properties, layout)
+        elif properties.options_type == 'paths':
+            self.draw_paths_tab(properties, layout)
+        elif properties.options_type == 'export':
+            self.draw_export_tab(properties, layout)
+        elif properties.options_type == 'import':
+            self.draw_import_tab(properties, layout)
+        elif properties.options_type == 'validations':
+            self.draw_validations_tab(properties, layout)
+
+    def draw_general_tab(self, properties, layout):
+        """
+        Draws all the properties in the General tab.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+        row.prop(properties, 'automatically_create_collections')
+
+    def draw_paths_tab(self, properties, layout):
+        """
+        Draws all the properties in the Paths tab.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+        row.prop(properties, 'path_mode', text='')
+        row = layout.row()
+        row.prop(properties, 'use_immediate_parent_collection_name')
+       
+        if properties.path_mode in ['send_to_unreal', 'both']:
             row = layout.row()
-            row.prop(properties, 'automatically_create_collections')
-
-        if properties.options_type == 'paths':
+            row.prop(properties, 'use_collections_as_folders')
+            # Mesh Folder (Unreal)
             row = layout.row()
-            row.prop(properties, 'path_mode', text='')
+            row.label(text='Mesh Folder (Unreal)')
+            # disable the mesh folder path input if a skeleton path is provided
             row = layout.row()
-            row.prop(properties, 'use_immediate_parent_collection_name')
-
-            if properties.path_mode in ['send_to_unreal', 'both']:
-                row = layout.row()
-                row.prop(properties, 'use_collections_as_folders')
-
-                # Mesh Folder (Unreal)
-                row = layout.row()
-                row.label(text='Mesh Folder (Unreal)')
-
-                # disable the mesh folder path input if a skeleton path is provided
-                row = layout.row()
-                row.enabled = not bool(properties.unreal_skeleton_asset_path)
-                row.alert = properties.incorrect_unreal_mesh_folder_path
-                row.prop(properties, 'unreal_mesh_folder_path', text='')
-                utilities.report_path_error_message(
+            row.enabled = not bool(properties.unreal_skeleton_asset_path)
+            row.alert = properties.incorrect_unreal_mesh_folder_path
+            row.prop(properties, 'unreal_mesh_folder_path', text='')
+            utilities.report_path_error_message(
                     layout,
                     properties.incorrect_unreal_mesh_folder_path,
                     validations.validate_unreal_path_by_property(
@@ -56,14 +75,13 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                         "incorrect_unreal_mesh_folder_path"
                     )
                 )
-
-                # Animation Folder (Unreal)
-                row = layout.row()
-                row.label(text='Animation Folder (Unreal)')
-                row = layout.row()
-                row.alert = properties.incorrect_unreal_animation_folder_path
-                row.prop(properties, 'unreal_animation_folder_path', text='')
-                utilities.report_path_error_message(
+            # Animation Folder (Unreal)
+            row = layout.row()
+            row.label(text='Animation Folder (Unreal)')
+            row = layout.row()
+            row.alert = properties.incorrect_unreal_animation_folder_path
+            row.prop(properties, 'unreal_animation_folder_path', text='')
+            utilities.report_path_error_message(
                     layout,
                     properties.incorrect_unreal_animation_folder_path,
                     validations.validate_unreal_path_by_property(
@@ -71,14 +89,13 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                         "incorrect_unreal_animation_folder_path"
                     )
                 )
-
-                # Skeleton Asset (Unreal)
-                row = layout.row()
-                row.label(text='Skeleton Asset (Unreal)')
-                row = layout.row()
-                row.alert = properties.incorrect_unreal_skeleton_path
-                row.prop(properties, 'unreal_skeleton_asset_path', text='')
-                utilities.report_path_error_message(
+            # Skeleton Asset (Unreal)
+            row = layout.row()
+            row.label(text='Skeleton Asset (Unreal)')
+            row = layout.row()
+            row.alert = properties.incorrect_unreal_skeleton_path
+            row.prop(properties, 'unreal_skeleton_asset_path', text='')
+            utilities.report_path_error_message(
                     layout,
                     properties.incorrect_unreal_skeleton_path,
                     validations.validate_unreal_path_by_property(
@@ -87,18 +104,16 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                     )
                 )
 
-            if properties.path_mode in ['export_to_disk', 'both']:
-
-                # Mesh Folder (Disk)
-                row = layout.row()
-                row.label(text='Mesh Folder (Disk)')
-
-                # disable the mesh folder path input if a skeleton path is provided
-                row.enabled = not bool(properties.unreal_skeleton_asset_path)
-                row = layout.row()
-                row.alert = properties.incorrect_disk_mesh_folder_path or properties.mesh_folder_untitled_blend_file
-                row.prop(properties, 'disk_mesh_folder_path', text='')
-                utilities.report_path_error_message(
+        if properties.path_mode in ['export_to_disk', 'both']:
+            # Mesh Folder (Disk)
+            row = layout.row()
+            row.label(text='Mesh Folder (Disk)')
+            # disable the mesh folder path input if a skeleton path is provided
+            row.enabled = not bool(properties.unreal_skeleton_asset_path)
+            row = layout.row()
+            row.alert = properties.incorrect_disk_mesh_folder_path or properties.mesh_folder_untitled_blend_file
+            row.prop(properties, 'disk_mesh_folder_path', text='')
+            utilities.report_path_error_message(
                     layout,
                     properties.incorrect_disk_mesh_folder_path,
                     validations.validate_disk_path_by_property(
@@ -106,7 +121,7 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                         "incorrect_disk_mesh_folder_path"
                     )
                 )
-                utilities.report_path_error_message(
+            utilities.report_path_error_message(
                     layout,
                     properties.mesh_folder_untitled_blend_file,
                     validations.validate_disk_path_by_property(
@@ -115,13 +130,13 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                     )
                 )
 
-                # Animation Folder (Disk)
-                row = layout.row()
-                row.label(text='Animation Folder (Disk)')
-                row = layout.row()
-                row.alert = properties.incorrect_disk_animation_folder_path or properties.animation_folder_untitled_blend_file
-                row.prop(properties, 'disk_animation_folder_path', text='')
-                utilities.report_path_error_message(
+            # Animation Folder (Disk)
+            row = layout.row()
+            row.label(text='Animation Folder (Disk)')
+            row = layout.row()
+            row.alert = properties.incorrect_disk_animation_folder_path or properties.animation_folder_untitled_blend_file
+            row.prop(properties, 'disk_animation_folder_path', text='')
+            utilities.report_path_error_message(
                     layout,
                     properties.incorrect_disk_animation_folder_path,
                     validations.validate_disk_path_by_property(
@@ -129,7 +144,7 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                         "incorrect_disk_animation_folder_path"
                     )
                 )
-                utilities.report_path_error_message(
+            utilities.report_path_error_message(
                     layout,
                     properties.animation_folder_untitled_blend_file,
                     validations.validate_disk_path_by_property(
@@ -138,114 +153,145 @@ class SendToUnrealPreferences(Send2UeProperties, Send2UeUIProperties, bpy.types.
                     )
                 )
 
-        if properties.options_type == 'export':
-            row = layout.row()
-            row.prop(properties, 'automatically_scale_bones')
-            row = layout.row()
-            row.prop(properties, 'export_all_actions')
-            row = layout.row()
-            row.prop(properties, 'auto_stash_active_action')
-            # this option is greyed out unless ue2rigify is active
-            row = layout.row()
-            row.enabled = bool(bpy.context.preferences.addons.get('ue2rigify'))
-            row.prop(properties, 'auto_sync_control_nla_to_source')
-            row = layout.row()
-            row.prop(properties, 'use_object_origin')
-            row = layout.row()
-            row.prop(properties, 'combine_child_meshes')
-            row = layout.row()
+    def draw_export_tab(self, properties, layout):
+        """
+        Draws all the properties in the Export tab.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+        row.prop(properties, 'automatically_scale_bones')
+        row = layout.row()
+        row.prop(properties, 'export_all_actions')
+        row = layout.row()
+        row.prop(properties, 'auto_stash_active_action')
 
-            # fbx export settings box
-            box = row.box()
-            row = box.row()
-            row.prop(
+        # this option is greyed out unless ue2rigify is active
+        row = layout.row()
+        row.enabled = bool(bpy.context.preferences.addons.get('ue2rigify'))
+        row.prop(properties, 'auto_sync_control_nla_to_source')
+        row = layout.row()
+        row.prop(properties, 'use_object_origin')
+        row = layout.row()
+        row.prop(properties, 'combine_child_meshes')
+        row = layout.row()
+
+        # fbx export settings box
+        box = row.box()
+        row = box.row()
+        row.prop(
                 properties,
                 'show_fbx_settings',
                 icon='TRIA_DOWN' if properties.show_fbx_settings else 'TRIA_RIGHT',
                 icon_only=True,
                 emboss=False
             )
-            row.label(text='FBX Settings', icon='EXPORT')
-            if properties.show_fbx_settings:
-                row = box.row()
-                row.label(text='Include:')
-                row = box.row()
-                row.prop(properties, 'use_custom_props')
-                row = box.row()
-                row.label(text='Transform:')
-                row = box.row()
-                row.prop(properties, 'global_scale')
-                row = box.row()
-                row.prop(properties, 'apply_scale_options')
-                row = box.row()
-                row.prop(properties, 'axis_forward')
-                row = box.row()
-                row.prop(properties, 'axis_up')
-                row = box.row()
-                row.prop(properties, 'apply_unit_scale')
-                row = box.row()
-                row.prop(properties, 'bake_space_transform')
-                row = box.row()
-                row.label(text='Geometry:')
-                row = box.row()
-                row.prop(properties, 'mesh_smooth_type')
-                row = box.row()
-                row.prop(properties, 'use_subsurf')
-                row = box.row()
-                row.prop(properties, 'use_mesh_modifiers')
-                row = box.row()
-                row.prop(properties, 'use_mesh_edges')
-                row = box.row()
-                row.prop(properties, 'use_tspace')
-                row = box.row()
-                row.label(text='Armature:')
-                row = box.row()
-                row.prop(properties, 'primary_bone_axis')
-                row = box.row()
-                row.prop(properties, 'secondary_bone_axis')
-                row = box.row()
-                row.prop(properties, 'armature_nodetype')
-                row = box.row()
-                row.prop(properties, 'use_armature_deform_only')
-                row = box.row()
-                row.prop(properties, 'add_leaf_bones')
-                row = box.row()
-                row.label(text='Animation:')
-                row = box.row()
-                row.prop(properties, 'bake_anim')
-                row = box.row()
-                row.prop(properties, 'bake_anim_use_all_bones')
-                row = box.row()
-                row.prop(properties, 'bake_anim_force_startend_keying')
-                row = box.row()
-                row.prop(properties, 'bake_anim_step')
-                row = box.row()
-                row.prop(properties, 'bake_anim_simplify_factor')
-                row = box.row()
-                row.label(text='Extras:')
-                row = box.row()
-                row.prop(properties, 'use_metadata')
+        row.label(text='FBX Settings', icon='EXPORT')     
 
-        if properties.options_type == 'validations':
-            row = layout.row()
-            row.prop(properties, 'validate_armature_transforms')
-            row = layout.row()
-            row.prop(properties, 'validate_materials')
-            row = layout.row()
-            row.prop(properties, 'validate_textures')
+        if properties.show_fbx_settings:
+            self.draw_fbx_settings(properties, box)
 
-        if properties.options_type == 'import':
-            row = layout.row()
-            row.prop(properties, 'import_materials')
-            row = layout.row()
-            row.prop(properties, 'import_textures')
-            row = layout.row()
-            row.prop(properties, 'import_animations')
-            row = layout.row()
-            row.prop(properties, 'import_lods')
-            row = layout.row()
-            row.prop(properties, 'import_sockets')
-            row = layout.row()
-            row.prop(properties, 'import_object_name_as_root')
-            row = layout.row()
-            row.prop(properties, 'advanced_ui_import')
+    def draw_fbx_settings(self, properties, layout):
+        """
+        Draws all the properties in the FBX Settings panel.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+
+        row.label(text='Include:')
+        row = layout.row()
+        row.prop(properties, 'use_custom_props')
+        row = layout.row()
+
+        row.label(text='Transform:')
+        row = layout.row()
+        row.prop(properties, 'global_scale')
+        row = layout.row()
+        row.prop(properties, 'apply_scale_options')
+        row = layout.row()
+        row.prop(properties, 'axis_forward')
+        row = layout.row()
+        row.prop(properties, 'axis_up')
+        row = layout.row()
+        row.prop(properties, 'apply_unit_scale')
+        row = layout.row()
+        row.prop(properties, 'bake_space_transform')
+        row = layout.row()
+
+        row.label(text='Geometry:')
+        row = layout.row()
+        row.prop(properties, 'mesh_smooth_type')
+        row = layout.row()
+        row.prop(properties, 'use_subsurf')
+        row = layout.row()
+        row.prop(properties, 'use_mesh_modifiers')
+        row = layout.row()
+        row.prop(properties, 'use_mesh_edges')
+        row = layout.row()
+        row.prop(properties, 'use_tspace')
+        row = layout.row()
+
+        row.label(text='Armature:')
+        row = layout.row()
+        row.prop(properties, 'primary_bone_axis')
+        row = layout.row()
+        row.prop(properties, 'secondary_bone_axis')
+        row = layout.row()
+        row.prop(properties, 'armature_nodetype')
+        row = layout.row()
+        row.prop(properties, 'use_armature_deform_only')
+        row = layout.row()
+        row.prop(properties, 'add_leaf_bones')
+        row = layout.row()
+
+        row.label(text='Animation:')
+        row = layout.row()
+        row.prop(properties, 'bake_anim')
+        row = layout.row()
+        row.prop(properties, 'bake_anim_use_all_bones')
+        row = layout.row()
+        row.prop(properties, 'bake_anim_force_startend_keying')
+        row = layout.row()
+        row.prop(properties, 'bake_anim_step')
+        row = layout.row()
+        row.prop(properties, 'bake_anim_simplify_factor')
+        row = layout.row()
+        
+        row.label(text='Extras:')
+        row = layout.row()
+        row.prop(properties, 'use_metadata')
+
+    def draw_import_tab(self, properties, layout):
+        """
+        Draws all the properties in the Import tab.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+        row.prop(properties, 'import_materials')
+        row = layout.row()
+        row.prop(properties, 'import_textures')
+        row = layout.row()
+        row.prop(properties, 'import_animations')
+        row = layout.row()
+        row.prop(properties, 'import_lods')
+        row = layout.row()
+        row.prop(properties, 'import_sockets')
+        row = layout.row()
+        row.prop(properties, 'import_object_name_as_root')
+        row = layout.row()
+        row.prop(properties, 'advanced_ui_import')
+
+    def draw_validations_tab(self, properties, layout):
+        """
+        Draws all the properties in the Validations tab.
+        :param properties: The add-on properties to use.
+        :param layout: The layout container for this tab.
+        """
+        row = layout.row()
+        row.prop(properties, 'validate_armature_transforms')
+        row = layout.row()
+        row.prop(properties, 'validate_materials')
+        row = layout.row()
+        row.prop(properties, 'validate_textures')
