8accd940dd60f004a9ed7032f32f2d69f0228a66
Jack Yao <jack.yao@polyhammer.com>
2024-06-14 09:31:16 -0500

    updated template names in test cases

--- tests/test_ue2rigify_mannequins.py
+++ tests/test_ue2rigify_mannequins.py
@@ -16,7 +16,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
         """
         self.run_modes_tests({
             'male_root': {
-                'template': 'male_mannequin',
+                'template': 'male_mannequin_UE4',
                 'modes': [
                     'SOURCE',
                     'FK_TO_SOURCE',
@@ -26,7 +26,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
                     ]
             },
             'female_root': {
-                'template': 'female_mannequin',
+                'template': 'female_mannequin_UE4',
                 'modes': [
                     'FK_TO_SOURCE',
                     'SOURCE_TO_DEFORM',
@@ -51,7 +51,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
                 },
                 'female_root': {
                     'new_template_name': 'test_mannequin',
-                    'starter_template_name': 'female_mannequin',
+                    'starter_template_name': 'female_mannequin_UE4',
                     'fk_to_source': {'upper_arm_fk.L': 'upperarm_l'},
                     'source_to_deform': {'upperarm_l': 'DEF-upper_arm.L'}
                 }
@@ -66,7 +66,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
         self.run_baking_tests({
             # TODO flip animation order and fix failure
             'male_root': {
-                'template': 'male_mannequin',
+                'template': 'male_mannequin_UE4',
                 'control_rig': 'rig',
                 'animations': ['third_person_run_01', 'third_person_walk_01'],
                 # 'bones': ['pelvis', 'calf_r', 'foot_l', 'hand_l'], # TODO make this pass with the hands and feet
@@ -80,7 +80,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
             },
             # TODO investigate female template fix failure
             # 'female_root': {
-            #     'template': 'female_mannequin',
+            #     'template': 'female_mannequin_UE4',
             #     'control_rig': 'rig',
             #     'animations': ['third_person_run_01', 'third_person_walk_01'],
             #     'bones': ['spine_02', 'calf_l', 'lowerarm_r'],
@@ -94,7 +94,7 @@ class TestUe2RigifyMannequins(BaseUe2RigifyTestCase):
         """
         self.run_template_sharing_tests({
             'male_root': {
-                'template': 'male_mannequin',
+                'template': 'male_mannequin_UE4',
             }
         })
 
