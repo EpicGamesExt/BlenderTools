b7fc8aff167cc0d2d7283ef41bffcea6723f209c
Jack Yao <jack.yao@polyhammer.com>
2024-06-14 10:29:51 -0500

    update the template folder check to account for blender version in test case

--- tests/utils/base_test_case.py
+++ tests/utils/base_test_case.py
@@ -1444,7 +1444,8 @@ class BaseUe2RigifyTestCase(BaseTestCase):
                 self.ue2rigify.constants.ToolInfo.NAME.value,
                 'resources',
                 'rig_templates',
-                f'{template_name}_test'
+                'b4_0' if int(os.environ.get('BLENDER_VERSION', '4.1').split('.')[0]) >= 4 else 'b3_6'
+                f'{template_name}_test'.lower()
             ]
 
             template_file_paths = [
